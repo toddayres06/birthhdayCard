d7cb0f5c87116bb5c14f751b8791481dcad87472
toddayres06 <107014840+toddayres06@users.noreply.github.com>
2022-11-01 15:10:17 -0500

    Add files via upload

---
+++ secondTry/index.js
@@ -0,0 +1,54 @@
+// let images = [
+//   "birthdayBox.jpg",
+//   "cruise.jpg",
+//   "bros.jpg"
+// ]
+
+// let images = document.getElementById("birthdayBox")
+let box = document.getElementById("images")
+
+// console.log(box)
+
+
+function picture() {
+  if (box === "birthdayBox") {
+    document.getElementById("images") = "cruise.jpg";
+  } else if (box ==="cruise.jpg") {
+    document.getElementById("images") = "bros.jpg";
+  }
+}
+
+
+
+function random(num) {
+    return Math.floor(Math.random()*num)
+  }
+  
+  function getRandomStyles() {
+    var r = random(255);
+    var g = random(255);
+    var b = random(255);
+    var mt = random(200);
+    var ml = random(50);
+    var dur = random(5)+5;
+    return `
+    background-color: rgba(${r},${g},${b},0.7);
+    color: rgba(${r},${g},${b},0.7); 
+    box-shadow: inset -7px -3px 10px rgba(${r-10},${g-10},${b-10},0.7);
+    margin: ${mt}px 0 0 ${ml}px;
+    animation: float ${dur}s ease-in infinite
+    `
+  }
+  
+  function createBalloons(num) {
+    var balloonContainer = document.getElementById("balloon-container")
+    for (var i = num; i > 0; i--) {
+    var balloon = document.createElement("div");
+    balloon.className = "balloon";
+    balloon.style.cssText = getRandomStyles();           balloonContainer.append(balloon);
+    }
+  }
+  
+  window.onload = function() {
+    createBalloons(100);
+  }
